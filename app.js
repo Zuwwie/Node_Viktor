a5b353020dfe8b7e8ec1d24fc698b48eb0ea4c72
Havronskyi Nazarii <nazar.gavronsky.mail@gmail.com>
2021-10-01 14:31:44 +0300

    update_2

--- app.js
+++ app.js
@@ -2,46 +2,42 @@ const fs = require(`fs`);
 const path = require(`path`);
 
 const defPath = path.join(__dirname, `data`);
+const gender = [`boys`, `girls`];
 
-fs.readdir(path.join(defPath), (err, data) => {
-    console.log(data);
-    if (err) {
-        console.log(err);
-        return;
-    }
-    data.forEach(dir => {
-        fs.readdir(path.join(defPath, dir), (err, data) => {
-            if (err) {
-                console.log(err);
-                return;
-            }
-            data.forEach(file => {
-                fs.readFile(path.join(defPath, dir, file), (err, data) => {
-                    if (err) {
-                        console.log(err);
-                        return;
-                    }
-                    const user = (JSON.parse(data));
-                    if (dir === "boys" && (user.gender === "Female" || user.gender === "female")) {
-                        fs.rename(path.join(defPath, dir, file),
-                            path.join(defPath, `girls`, file),
-                            (err) => {
-                                if (err) {
-                                    console.log(err);
-                                }
-                            });
-                    }
-                    if (dir === "girls" && (user.gender === "Male" || user.gender === "male")) {
-                        fs.rename(path.join(defPath, dir, file),
-                            path.join(defPath, `boys`, file),
-                            (err) => {
-                                if (err) {
-                                    console.log(err);
-                                }
-                            });
-                    }
-                });
+
+gender.forEach(dir => {
+    fs.readdir(path.join(defPath, dir), (err, data) => {
+        if (err) {
+            console.log(err);
+            return;
+        }
+        data.forEach(file => {
+            fs.readFile(path.join(defPath, dir, file), (err, data) => {
+                if (err) {
+                    console.log(err);
+                    return;
+                }
+                const user = (JSON.parse(data));
+                if (dir === "boys" && (user.gender === "Female" || user.gender === "female")) {
+                    fs.rename(path.join(defPath, dir, file),
+                        path.join(defPath, `girls`, file),
+                        (err) => {
+                            if (err) {
+                                console.log(err);
+                            }
+                        });
+                }
+                if (dir === "girls" && (user.gender === "Male" || user.gender === "male")) {
+                    fs.rename(path.join(defPath, dir, file),
+                        path.join(defPath, `boys`, file),
+                        (err) => {
+                            if (err) {
+                                console.log(err);
+                            }
+                        });
+                }
             });
         });
     });
 });
+
