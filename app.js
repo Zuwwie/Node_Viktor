eb6b601ef8e75a4ade627d00ae2f194bca0e13aa
Havronskyi Nazarii <nazar.gavronsky.mail@gmail.com>
2021-10-01 18:46:05 +0300

    update_3

--- app.js
+++ app.js
@@ -4,32 +4,32 @@ const path = require(`path`);
 const defPath = path.join(__dirname, `data`);
 const gender = [`boys`, `girls`];
 
-
 gender.forEach(dir => {
     fs.readdir(path.join(defPath, dir), (err, data) => {
         if (err) {
             console.log(err);
             return;
         }
+
         data.forEach(file => {
             fs.readFile(path.join(defPath, dir, file), (err, data) => {
                 if (err) {
                     console.log(err);
                     return;
                 }
+
                 const user = (JSON.parse(data));
+
                 if (dir === "boys" && (user.gender === "Female" || user.gender === "female")) {
-                    fs.rename(path.join(defPath, dir, file),
-                        path.join(defPath, `girls`, file),
-                        (err) => {
-                            if (err) {
-                                console.log(err);
-                            }
-                        });
+                    changeFile(`girls`);
                 }
                 if (dir === "girls" && (user.gender === "Male" || user.gender === "male")) {
+                    changeFile(`boys`);
+                }
+
+                function changeFile(userGender) {
                     fs.rename(path.join(defPath, dir, file),
-                        path.join(defPath, `boys`, file),
+                        path.join(defPath, userGender, file),
                         (err) => {
                             if (err) {
                                 console.log(err);
