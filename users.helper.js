de873576c00d612fd267161b0280eccdfeea72ed
Havronskyi Nazarii <nazar.gavronsky.mail@gmail.com>
2021-10-06 11:51:28 +0300

    Up_1

--- users.helper.js
+++ users.helper.js
@@ -11,48 +11,70 @@ const writeFile = (newArr) => {
         }
         console.log(`file written successfully`)
     });
-}
+};
 
-const createUser = (newUser) => {
-    fs.readFile(defPath, (err, data) => {
-        if (err) {
-            console.log(err);
-            return;
-        }
+readFile = () => {
+    return new Promise((res, rej) => {
+        fs.readFile(defPath,
+            (err, data) => {
+                if (err) {
+                    console.log(err);
+                    rej(err);
+                    return
+                }
 
-        const users = (JSON.parse(data));
-
-        newUser.id = users[users.length - 1].id + 1;
-
-        users.push(newUser);
-
-        writeFile(users);
+                const users = JSON.parse(data);
+                console.log(users);
+                res(users);
+            }
+        );
     });
 };
 
-const deleteUser = (user_id) => {
-    fs.readFile(defPath, (err, data) => {
-        if (err) {
-            console.log(err);
-            return;
+const createUser = async (newUser) => {
+    // fs.readFile(defPath, (err, data) => {
+    //     if (err) {
+    //         console.log(err);
+    //         return;
+    //     }
+    //
+    //     const users = (JSON.parse(data));
+
+    const users = await readFile();
+
+    newUser.id = users[users.length - 1].id + 1;
+
+    users.push(newUser);
+
+    writeFile(users);
+    // });
+};
+
+const deleteUser = async (user_id) => {
+    // fs.readFile(defPath, (err, data) => {
+    //     if (err) {
+    //         console.log(err);
+    //         return;
+    //     }
+    //
+    //     const users = (JSON.parse(data));
+
+    const users = await readFile();
+
+    for (let i = 0; i < users.length; i++) {
+
+        const user = users[i];
+
+        if (user.id == user_id) {
+            users.splice(i, 1);
+            break;
         }
+    }
+    console.log(users);
 
-        const users = (JSON.parse(data));
+    writeFile(users);
 
-        for (let i = 0; i < users.length; i++) {
-
-            const user = users[i];
-
-            if (user.id == user_id) {
-                users.splice(i, 1);
-                break;
-            }
-        }
-        console.log(users);
-
-        writeFile(users);
-
-    });
+    // });
 };
 
 module.exports = {createUser, deleteUser}
