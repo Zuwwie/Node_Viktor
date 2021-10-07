77e2fc95638b429dc686d3969a0f7c57e918b4b8
Havronskyi Nazarii <nazar.gavronsky.mail@gmail.com>
2021-10-08 00:28:57 +0300

    DZ-3 v2 work (add auth.middleware)

--- routes/auth.router.js
+++ routes/auth.router.js
@@ -1,7 +1,8 @@
 const router = require('express').Router();
 
 const authController = require('../controllers/auth.controller');
+const authMiddleware = require('../middlewares/auth.middleware');
 
-router.post('/login', authController.login);
+router.post('/login', authMiddleware.userAuthMiddleware, authController.login);
 
 module.exports = router;
